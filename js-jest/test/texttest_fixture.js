2539e8394f32244eb2d850d8854296489f845639
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-02-23 10:10:07 +0100

    first commit

--- js-jest/test/texttest_fixture.js
+++ js-jest/test/texttest_fixture.js
@@ -1,18 +1,18 @@
+const { Shop, CommonItem, AgedBrie, Sulfuras, BackstagePass, ConjuredItem } = require("../src/gilded_rose");
 
-const { Shop, Item } = require("../src/gilded_rose");
-
+/// Reminder: repesctive arguments are name, sellIn and quality
 const items = [
-  new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
-  new Item("Elixir of the Mongoose", 5, 7),
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new CommonItem("+5 Dexterity Vest", 10, 20),
+  new AgedBrie("Aged Brie", 2, 0),
+  new CommonItem("Elixir of the Mongoose", 5, 7),
+  new Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
+  new Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80),
+  new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 5, 49),
 
   // This Conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6),
+  new ConjuredItem("Conjured Mana Cake", 3, 6),
 ];
 
 const days = Number(process.argv[2]) || 2;
